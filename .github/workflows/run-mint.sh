5300682602ebc1c242b581338bcb87fe45266117
wildone <max.barrass@gmail.com>
2026-08-05 03:07:42 +1000

    ci: pin archived server Mint compatibility suite

--- .github/workflows/run-mint.sh
+++ .github/workflows/run-mint.sh
@@ -7,16 +7,18 @@ export ACCESS_KEY="$2"
 export SECRET_KEY="$3"
 export JOB_NAME="$4"
 export MINT_MODE="full"
+export MINT_IMAGE="docker.io/minio/mint@sha256:f80ec8f981a5b54d98c1bbbbe7a191e5a2c696c5f45f4d18f712c418cec6e61d"
 
 docker system prune -f || true
 docker volume prune -f || true
-docker volume rm $(docker volume ls -f dangling=true) || true
+docker volume ls -q -f dangling=true | xargs -r docker volume rm || true
 
 ## change working directory
 cd .github/workflows/mint
 
-## always pull latest
-docker pull docker.io/minio/mint:edge
+# The upstream server is archived, so use the last stable Mint release rather
+# than the moving edge tag, whose future S3 assertions can outpace this server.
+docker pull "${MINT_IMAGE}"
 
 docker compose -f minio-${MODE}.yaml up -d
 sleep 1m
@@ -39,7 +41,7 @@ docker run --rm --net=mint_default \
 	-e SECRET_KEY="${SECRET_KEY}" \
 	-e ENABLE_HTTPS=0 \
 	-e MINT_MODE="${MINT_MODE}" \
-	docker.io/minio/mint:edge
+	"${MINT_IMAGE}"
 
 # FIXME: enable this after fixing aws-sdk-java-v2 tests
 # # unpause the node, to check that all S3 calls work while one node goes wrong
@@ -51,7 +53,7 @@ docker run --rm --net=mint_default \
 # 	-e SECRET_KEY="${SECRET_KEY}" \
 # 	-e ENABLE_HTTPS=0 \
 # 	-e MINT_MODE="${MINT_MODE}" \
-# 	docker.io/minio/mint:edge
+# 	"${MINT_IMAGE}"
 
 docker compose -f minio-${MODE}.yaml down || true
 sleep 10s
